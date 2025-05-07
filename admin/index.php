e7bbf0fdfb21e490e5e89b37b2cf9f9885303042
admin_uts <dptopasantesytesistasUTS@gmail.com>
2025-05-06 22:43:06 -0400

    Ajustes del redireccionamiento del login

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+#Llamada para las funciones globales para configurar estilos
+include ('../app/config.php');
+include ('../admin/layout/parte1.php');
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <br>
+    <div class="content">
+    <div class="container-fluid">
+        <div class="row">
+        <h1>Vista principal</h1>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include ('../admin/layout/parte2.php');
+include ('../layout/mensajes.php');
+?>
--- login/controller_login.php
+++ login/controller_login.php
@@ -5,7 +5,7 @@ include ('../app/config.php');
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM usuarios WHERE email = '$email';";
+$sql = "SELECT * FROM usuarios WHERE email = '$email' AND estado = '1';";
 $query = $pdo->prepare($sql);
 $query->execute();
 
@@ -20,10 +20,27 @@ foreach ($usuarios as $usuario) {
     $contador = $contador +1;
 }
 
+// Módulo para verificar si un usuario es válido
+
 if ($usuario && $password === $usuario['password']) {
-    header('Location: ' . APP_URL . '/admin');
-    exit();
+    echo "los datos son correctos";
+    session_start();
+    $_SESSION['mensaje'] = "Bienvenido al sistema";
+    $_SESSION['icono'] = "success";
+    header('Location:'.APP_URL.'/admin');
 } else {
-    header('Location: ' . APP_URL . '/login?error=credenciales_incorrectas');
-    exit();
+    echo "los datos son in correctos";
+    session_start();
+    $_SESSION['mensaje'] = "Los datos introducidos son incorrectos, vuelva a intentarlo";
+    header('Location:'.APP_URL.'/login');
 }
+
+//cambio para verificar contraseñas encriptadas
+// $2y$10$0tYmdHU9uGCIxY1f90W1EuIm54NQ8axowkxL1WzLbqO2LdNa8m3l2
+// if (($contador > 0 ) && ($password_verify ($password, $password_verify))) {
+//     header('Location: ' . APP_URL . '/admin');
+//     exit();
+// } else {
+//     header('Location: ' . APP_URL . '/login?error=credenciales_incorrectas');
+//     exit();
+// }
